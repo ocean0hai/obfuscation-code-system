84659bb0d8cf9de902dcb1c4f9058f2812b62466
ocean0hai <3067451183@qq.com>
2023-06-11 17:52:41 +0800

    fix:add  a file

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
   server: {
     //中转服务器
     port: 8800, // 你需要定义的端口号
-    // origin: "http://127.0.0.1:8800",
+    origin: "http://127.0.0.1:8800",
     host:'0.0.0.0',
     proxy: {
       //通过代理实现跨域
       "/api": {
-        target: "http://8.130.113.197", //替换的服务器地址
+        target: "http://8.130.113.197:8080", //替换的服务器地址
         changeOrigin: true, //开启代理，允许跨域
         // 重写路径 --> 作用与vue配置pathRewrite作用相同
         rewrite: (path) => path.replace(/^\/api/, ""),
